c17acc59bfd6ffa32f6e206af69d881a244b72ee
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-12-01 10:14:23 -0500

    Working delete form method

--- src/views/js/form.js
+++ src/views/js/form.js
@@ -1,22 +1,46 @@
 document.getElementById('form-accept').addEventListener('submit', async (e) => {
     e.preventDefault()
-    const id = document.getElementById('form-id').value
-    const NOM_SOL = document.getElementById('form-name').value
-    const EST_SOL = true
-    const db = JSON.stringify({ id, NOM_SOL, EST_SOL })
-    console.log(db)
+    const data = [
+        document.getElementById('form-id').value,
+        document.getElementById('form-name').value,
+        true
+    ]
     try {
         const response = await fetch('/form', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ id, NOM_SOL, EST_SOL })
+            body: JSON.stringify(data)
         })
         const result = await response.json()
-        console.log(result)
-        window.alert('EXITO')
+        window.alert(result.error ? 'ERROR' : 'EXITO')
         location.reload()
+    } catch (error) {
+        console.error('Error al enviar la solicitud:', error);
+        window.alert('Error al enviar los datos');
+    }
+})
+
+
+document.getElementById('form-delete').addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const data = [
+        document.getElementById('form-id').value,
+        document.getElementById('form-name').value,
+        true
+    ]
+    try {
+        const response = await fetch('/form', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        const result = await response.json()
+        window.alert(result.error ? 'ERROR' : 'EXITO')
+        window.location.href = "/forms"
     } catch (error) {
         console.error('Error al enviar la solicitud:', error);
         window.alert('Error al enviar los datos');
