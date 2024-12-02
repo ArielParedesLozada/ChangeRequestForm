65b6b522d3211a9c8b41b9ae124bb4ddcbe7e5fa
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-12-01 20:22:56 -0500

    Display projects and personel information to create and update a change request

--- src/views/js/all.js
+++ src/views/js/all.js
@@ -1,3 +1,5 @@
+//const { response } = require("express")
+
 document.getElementById('new-form').addEventListener('submit', async (e) => {
     e.preventDefault()
     const data = [
@@ -32,4 +34,30 @@ document.getElementById('new-form').addEventListener('submit', async (e) => {
         console.error('Error al enviar la solicitud:', error);
         window.alert('Error al enviar los datos');
     }
+})
+
+fetch('/personal')
+.then(response => response.json())
+.then(data => {
+    const select = document.getElementById('form-solicitante') 
+    let selectText = select.innerHTML
+    for (let i = 0; i < data.body.length; i++) {
+        selectText += `
+            <option value="${data.body[i][0]}">${data.body[i][1]} ${data.body[i][2]}</option>
+        `
+    }
+    select.innerHTML = selectText
+})
+
+fetch('/projects')
+.then(response => response.json())
+.then(data => {
+    const select = document.getElementById('form-project') 
+    let selectText = select.innerHTML
+    for (let i = 0; i < data.body.length; i++) {
+        selectText += `
+            <option value="${data.body[i][0]}">${data.body[i][1]}</option>
+        `
+    }
+    select.innerHTML = selectText
 })
